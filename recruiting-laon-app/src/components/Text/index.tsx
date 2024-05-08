1b687e08b6dd0844f641e640e4e62a4340c9f5c6
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-08 07:01:22 -0300

    [Feat] Criando componente de texto

---
+++ recruiting-laon-app/src/components/Text/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Text as RNText, type TextProps } from "react-native";
+import { ColorTheme } from "../../theme/ColorTheme";
+import { FontsTheme } from "../../theme/FontsTheme";
+
+type TextType =
+  | "semibold_40"
+  | "semibold_24"
+  | "semibold_16"
+  | "regular_16"
+  | "regular_12"
+  | "semibold_12";
+
+type TypographyProps = TextProps & {
+  textType: TextType;
+  color?: string;
+  error?: boolean;
+};
+
+export function Text({
+  textType,
+  children,
+  color = ColorTheme.gray.gray_500,
+  error = false,
+  ...rest
+}: TypographyProps) {
+  function textStyles() {
+    return FontsTheme[textType];
+  }
+
+  return (
+    <RNText
+      {...rest}
+      style={[
+        { color: error ? ColorTheme.feedback.negative : color },
+        textStyles(),
+        rest.style,
+      ]}
+    >
+      {children}
+    </RNText>
+  );
+}
