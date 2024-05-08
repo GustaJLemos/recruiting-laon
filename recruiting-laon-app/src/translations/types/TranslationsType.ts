fa61e24199f426ea1acd25f7c3f98968155ace34
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-08 08:22:20 -0300

    [Feat] Criando tipagem das traduções gerais

---
+++ recruiting-laon-app/src/translations/types/TranslationsType.ts
@@ -0,0 +1,13 @@
+import { CommonsTranslations } from "./CommonsTranslations";
+import { LoginScreenTranslations } from "./LoginScreenTranslations";
+
+export type TranslationsType = {
+  en: {
+    LoginScreen: LoginScreenTranslations;
+    Commons: CommonsTranslations;
+  };
+  ptbr: {
+    LoginScreen: LoginScreenTranslations;
+    Commons: CommonsTranslations;
+  };
+};
