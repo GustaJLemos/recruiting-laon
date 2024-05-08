b0f995344047b751a5fe7cb5d31cb98316454fd2
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-08 08:21:46 -0300

    [Feat] Criando traduções da tela de login

---
+++ recruiting-laon-app/src/translations/LoginScreen/en.ts
@@ -0,0 +1,16 @@
+import { LoginScreenTranslations } from "../types/LoginScreenTranslations";
+
+export const LoginScreenEn: LoginScreenTranslations = {
+  toast: {
+    success: "Registration completed! Enjoy the app.",
+    error: "We were unable to register. Please review your details and try again!",
+  },
+  title: "Register",
+  subtitle: "Follow the best films and series.",
+  placeholderInput: {
+    name: "Full name",
+    email: "Email",
+    password: "Password",
+  },
+  warningText: "By clicking register, you are accepting Laon's Terms and Conditions and Privacy Policy.",
+}
---
+++ recruiting-laon-app/src/translations/LoginScreen/pt.ts
@@ -0,0 +1,16 @@
+import { LoginScreenTranslations } from "../types/LoginScreenTranslations";
+
+export const LoginScreenPt: LoginScreenTranslations = {
+  toast: {
+    success: "Cadastro realizado! Aproveite o app.",
+    error: "Não foi possível realizar seu cadastro. Revise os dados e tente novamente!",
+  },
+  title: "Cadastre-se",
+  subtitle: "Acompanhe os melhores filmes e séries.",
+  placeholderInput: {
+    name: "Nome completo",
+    email: "Email",
+    password: "Senha",
+  },
+  warningText: "Ao clicar em cadastrar, você está aceitando os Termos e Condições e a Política de Privacidade da Laon.",
+}
