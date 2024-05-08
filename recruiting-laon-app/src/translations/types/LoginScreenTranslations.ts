eea3c2f561ae6910ac36eb7f5fa0882a5e1e69fd
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-08 08:22:10 -0300

    [Feat] Criando tipagem das traduções de login

---
+++ recruiting-laon-app/src/translations/types/LoginScreenTranslations.ts
@@ -0,0 +1,14 @@
+export type LoginScreenTranslations = {
+  toast: {
+    success: string;
+    error: string;
+  },
+  title: string;
+  subtitle: string;
+  placeholderInput: {
+    name: string;
+    email: string;
+    password: string;
+  },
+  warningText: string;
+}
