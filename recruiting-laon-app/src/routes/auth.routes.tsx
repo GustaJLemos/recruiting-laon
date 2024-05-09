e7852d716f14199d00941efcbcd7b0fd9c573c09
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-08 22:18:49 -0300

    [Feat] Verificações para renderizar as telas

--- recruiting-laon-app/src/routes/auth.routes.tsx
+++ recruiting-laon-app/src/routes/auth.routes.tsx
@@ -4,6 +4,7 @@ import {
 } from "@react-navigation/native-stack";
 import { Login } from "../screens/auth/Login";
 import { Register } from "../screens/auth/Register";
+import { userStore } from "../stores/userStore";
 
 type AuthRoutes = {
   LoginScreen: undefined;
@@ -15,8 +16,15 @@ const Stack = createNativeStackNavigator<AuthRoutes>();
 export type AuthRoutesProps = NativeStackNavigationProp<AuthRoutes>;
 
 export function AuthRoutes() {
+  const userAlreadyRegistered = userStore(
+    (store) => store.userAlreadyRegistered
+  );
+
   return (
     <Stack.Navigator
+      initialRouteName={
+        userAlreadyRegistered ? "LoginScreen" : "RegisterScreen"
+      }
       screenOptions={{
         headerShown: false,
       }}
