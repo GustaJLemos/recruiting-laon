08facd39b225844a2e4a4969ec7271a50a426955
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-09 07:19:14 -0300

    [Feat] Adicionando tradução do login para as traduções globais

--- recruiting-laon-app/src/translations/index.tsx
+++ recruiting-laon-app/src/translations/index.tsx
@@ -1,5 +1,7 @@
 import { CommonsEn } from "./Commons/en";
 import { CommonsPt } from "./Commons/pt";
+import { LoginScreenEn } from "./LoginScreen/en";
+import { LoginScreenPt } from "./LoginScreen/pt";
 import { RegisterScreenEn } from "./RegisterScreen/en";
 import { RegisterScreenPt } from "./RegisterScreen/pt";
 import { TranslationsType } from "./types/TranslationsType";
@@ -7,10 +9,12 @@ import { TranslationsType } from "./types/TranslationsType";
 export const translations: TranslationsType = {
   en: {
     RegisterScreen: RegisterScreenEn,
+    LoginScreen: LoginScreenEn,
     Commons: CommonsEn,
   },
   ptbr: {
     RegisterScreen: RegisterScreenPt,
+    LoginScreen: LoginScreenPt,
     Commons: CommonsPt,
   },
 };
