fb5138964439326d59cea562e1dc862b7d4316fd
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-09 07:19:30 -0300

    [Feat] Criando as traduções do login

---
+++ recruiting-laon-app/src/translations/LoginScreen/en.ts
@@ -0,0 +1,14 @@
+import { LoginScreenTranslations } from "../types/LoginScreenTranslations";
+
+export const LoginScreenEn: LoginScreenTranslations = {
+  toast: {
+    success: "Logged in!",
+    error: "Unable to log in. Please review your details and try again!",
+  },
+  title: "Enter",
+  subtitle: "Welcome back!",
+  placeholderInput: {
+    email: "Email",
+    password: "Password",
+  },
+}
---
+++ recruiting-laon-app/src/translations/LoginScreen/pt.ts
@@ -0,0 +1,14 @@
+import { LoginScreenTranslations } from "../types/LoginScreenTranslations";
+
+export const LoginScreenPt: LoginScreenTranslations = {
+  toast: {
+    success: "Login efetuado!",
+    error: "Não foi possível efetuar o login. Revise os dados e tente novamente!",
+  },
+  title: "Entrar",
+  subtitle: "Bem vindo(a) de volta!",
+  placeholderInput: {
+    email: "Email",
+    password: "Senha",
+  },
+}
