bf008a92076ec8fda63af62a501e9a660b4c8c86
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-08 22:20:13 -0300

    [Feat] Adicionando variavel de controle para verificar se o usuário já se registrou no app

--- recruiting-laon-app/src/stores/userStore.ts
+++ recruiting-laon-app/src/stores/userStore.ts
@@ -4,10 +4,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type UserStoreProps = {
   userIsSigned: boolean;
+  userAlreadyRegistered: boolean;
   // Fiz só para "imitar" um login com jwt
   userAccessToken: string;
   refreshToken: string;
   signIn: (token: string, refreshToken: string) => void;
+  register: (token: string, refreshToken: string) => void;
   signOut: () => void;
 };
 
@@ -15,11 +17,18 @@ export const userStore = create(
   persist<UserStoreProps>(
     (set, get) => ({
       userIsSigned: false,
+      userAlreadyRegistered: false,
       userAccessToken: "",
       refreshToken: "",
       signIn: (token: string, refreshToken: string) => {
         set({ userIsSigned: true });
         set({ userAccessToken: token, refreshToken });
+      },
+      register: (token: string, refreshToken: string) => {
+        const signIn = get().signIn;
+
+        signIn(token, refreshToken)
+        set({ userAlreadyRegistered: true });
       },
       signOut: () => {
         set({ userAccessToken: "", refreshToken: "" });
