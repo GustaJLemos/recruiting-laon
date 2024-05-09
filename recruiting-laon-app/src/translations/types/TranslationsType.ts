b632d5dbddbe1861d38021ccc1d252b3efa910fe
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-09 07:19:50 -0300

    [Feat] Tipagem das traduções

--- recruiting-laon-app/src/translations/types/TranslationsType.ts
+++ recruiting-laon-app/src/translations/types/TranslationsType.ts
@@ -1,13 +1,16 @@
 import { CommonsTranslations } from "./CommonsTranslations";
+import { LoginScreenTranslations } from "./LoginScreenTranslations";
 import { RegisterScreenTranslations } from "./RegisterScreenTranslations";
 
 export type TranslationsType = {
   en: {
     RegisterScreen: RegisterScreenTranslations;
+    LoginScreen: LoginScreenTranslations;
     Commons: CommonsTranslations;
   };
   ptbr: {
     RegisterScreen: RegisterScreenTranslations;
+    LoginScreen: LoginScreenTranslations;
     Commons: CommonsTranslations;
   };
 };
