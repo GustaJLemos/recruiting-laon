23a61eaf9821f5426f2459e8329a367ae4aa1b66
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-10 14:38:13 -0300

    [Feat] Criando tipagem das traduções de detalhes

---
+++ recruiting-laon-app/src/translations/types/DetailsScreenTranslations.ts
@@ -0,0 +1,13 @@
+export type DetailsScreenTranslations = {
+  originalTitle: string;
+  year: string;
+  reviewsNumber: string;
+  synopsis: string;
+  cast: string;
+  castMock: string;
+  awards: string;
+  awardsMock: string;
+  director: string;
+  directorMock: string;
+  reviews: string;
+}
