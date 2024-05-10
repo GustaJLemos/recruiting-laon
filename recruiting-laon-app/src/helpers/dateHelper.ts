ae2c44bb87a6dbd09f11b10db048c7a984aefacb
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-10 14:33:25 -0300

    [Feat] Adicionando data no formato americano

--- recruiting-laon-app/src/helpers/dateHelper.ts
+++ recruiting-laon-app/src/helpers/dateHelper.ts
@@ -3,3 +3,7 @@ import { format } from "date-fns";
 export const datetimePtBr = (date: Date) => {
   return format(new Date(date), "dd/MM/yyyy");
 };
+
+export const datetimeEnUs = (date: Date) => {
+  return format(new Date(date), "MM/dd/yyyy");
+};
