6132033be3ff7803a57c3b417b520d04a0129913
GustaJLemos <gustavolemosmendes@gmail.com>
2024-05-10 14:37:42 -0300

    [Feat] Traduções dos detales

---
+++ recruiting-laon-app/src/translations/DetailsScreen/en.ts
@@ -0,0 +1,15 @@
+import { DetailsScreenTranslations } from "../types/DetailsScreenTranslations";
+
+export const DetailsScreenEn: DetailsScreenTranslations = {
+  originalTitle: "Original title:",
+  year: "Year:",
+  reviewsNumber: "Number of reviews:",
+  synopsis: "Synopsis",
+  cast: "Cast",
+  castMock: "Carey Mulligan, Bo Burnham, Alison Brie, Laverne Cox, Jennifer Coolidge and others.",
+  awards: "Awards",
+  awardsMock: "Oscar for Best Original Screenplay, Critics Choice Award for Best Actress and others.",
+  director: "Director",
+  directorMock: "Emerald Fennell",
+  reviews: "Reviews",
+}
---
+++ recruiting-laon-app/src/translations/DetailsScreen/pt.ts
@@ -0,0 +1,15 @@
+import { DetailsScreenTranslations } from "../types/DetailsScreenTranslations";
+
+export const DetailsScreenPt: DetailsScreenTranslations = {
+  originalTitle: "Titulo original:",
+  year: "Ano:",
+  reviewsNumber: "Quantidade de avaliações:",
+  synopsis: "Sinopse",
+  cast: "Elenco",
+  castMock: "Carey Mulligan, Bo Burnham, Alison Brie, Laverne Cox, Jennifer Coolidge e outros.",
+  awards: "Prêmios",
+  awardsMock: "Oscar de Melhor Roteiro Original, Critics Choice Award de Melhor Atriz e outros.",
+  director: "Diretor",
+  directorMock: "Emerald Fennell",
+  reviews: "Avaliações",
+}
